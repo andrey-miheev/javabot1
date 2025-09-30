b80451ea3493b38c31991eb19b9f2bfa84d2da4f
FyodorGV <fedorgreben17@mail.ru>
2025-09-30 14:08:55 +0500

    Add files via upload
    make tests for bot

--- javabot1/src/test/java/com/task1/javabot1/Javabot1ApplicationTests.java
+++ javabot1/src/test/java/com/task1/javabot1/Javabot1ApplicationTests.java
@@ -1,13 +1,31 @@
 package com.task1.javabot1;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
-class Javabot1ApplicationTests {
+class UpdateConsumerTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void testEchoCommand() {
+        UpdateConsumer consumer = new UpdateConsumer();
+        assertEquals("Твое сообщение: TEST", consumer.processUserInput("TEST"));
+    }
 
+    @Test
+    void testStartCommand() {
+        UpdateConsumer consumer = new UpdateConsumer();
+        String result = consumer.processUserInput("/start");
+
+        assertTrue(result.contains("Привет!"));
+        assertTrue(result.contains("Я эхо-бот."));
+        assertTrue(result.contains("Повторять твои сообщения"));
+    }
+
+    @Test
+    void testHelpCommand() {
+        UpdateConsumer consumer = new UpdateConsumer();
+        assertEquals("Вот что я умею:\n/start - познакомиться со мной\n/help  - список всех команд\n(любое другое сообщение я повторю тебе обратно)\n",
+                consumer.processUserInput("/help"));
+    }
 }
